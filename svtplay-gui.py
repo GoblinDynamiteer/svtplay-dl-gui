e6aa22e9545260e001dc046361275e138c9e80ea
Johan Kämpe <kampe.johan@gmail.com>
2018-02-22 20:34:29 +0100

    Cleanup, fix default output directory
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- svtplay-gui.py
+++ svtplay-gui.py
@@ -8,25 +8,22 @@ def press(button):
     else:
         link = app.getEntry("Link")
         path = app.getEntry("Output Dir")
+        if path is "":
+            path = svtplay_path
         print("Link:", link)
         print("Dir: ", path)
         os.system("svtplay-dl " + " -S --remux -o " + path + " " + link)
 
+svtplay_path = "C:\\svtplay-dl\\"
+
 # create a GUI variable called app
 app = gui("svtplay-dl GUI", "300x400")
 app.setPadding([20,20])
-svtplay_path = "C:\\svtplay-dl\\"
-# add & configure widgets - widgets get a name, to help referencing them later
+
 app.addLabel("title", "svtplay-dl")
-#app.setLabelBg("title", "red")
-# start the GUI
-
 app.addLabelEntry("Link")
-
-app.addLabel("l1", "Label 1")
 app.addDirectoryEntry("Output Dir")
 app.setEntryDefault("Output Dir", svtplay_path)
-#app.setDirectoryEntryWidth("dirinput", 300)
 
 # link the buttons to the function called press
 app.addButtons(["Submit", "Cancel"], press)
