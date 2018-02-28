cc58d5b72cf4d6130569148514a23445914c052d
Johan Kämpe <kampe.johan@gmail.com>
2018-02-28 21:24:27 +0100

    Run svtplay-dl as subprocess
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- svtplay-gui.py
+++ svtplay-gui.py
@@ -1,7 +1,25 @@
 # import the library
 from appJar import gui
 from config import get_setting
+import subprocess
 import os
+
+class svplay_dl:
+    def __init__(self, dl_dir, get_subs, remux):
+        self.process = None;
+        self.dl_dir = dl_dir
+        self.remux = remux
+        self.get_subs = get_subs
+    def run(self, link):
+        if self.process and self.check_process():
+            print("Process running!")
+            return
+        self.process = subprocess.Popen("svtplay-dl " + \
+            " -S --remux -o " + self.dl_dir + " " + link)
+    def check_process(self):
+        if self.process.poll() is None:
+            return True
+        return False
 
 def submit_button_press(button):
     if button == "Cancel":
@@ -13,9 +31,10 @@ def submit_button_press(button):
             path = default_download_path
         if not link:
             return
-        os.system("svtplay-dl " + " -S --remux -o " + path + " " + link)
+        svpdl.run(link)
 
 default_download_path = get_setting("paths", "defaultpath")
+svpdl = svplay_dl(default_download_path, get_subs=True, remux=True)
 
 app = gui("svtplay-dl GUI", "300x400")
 app.setPadding([20,20])
